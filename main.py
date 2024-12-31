06d10b456031b95a65fbd44ba1778997ba987bb1
v_pycopeng <v_pycopeng@tencent.com>
2024-12-31 14:52:02 +0800

    接口添加文档注释

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from fastapi import FastAPI
 
-app=FastAPI()
+app=FastAPI(title="学习FastApi测试接口",deprecated="项目描述")
 
 
-@app.get("/")
+@app.get("/",summary="首页接口注释",description="首页接口描述",tags=["Index"])
 def Index():
     return "Hello FastApi!"
 
